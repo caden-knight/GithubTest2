3418da975a37077eea6b67191453b25d85fd1de2
cadenknight <cadenknight@gmail.com>
2022-03-15 20:33:02 -0600

    made a comment change

--- GithubTest/GameScene.swift
+++ GithubTest/GameScene.swift
@@ -15,7 +15,7 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
+        // Get label node from scene and store it for use later [here is my change]
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
             label.alpha = 0.0
